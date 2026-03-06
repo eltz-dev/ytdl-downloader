a9fd448b367a3ca265f1475d0a100fa06a81c215
eltz-dev <eltzjb@gmail.com>
2026-03-06 23:23:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 
+async function convert(type){
+  const link = document.getElementById("ytlink").value.trim()
+  const status = document.getElementById("status")
+  const box = document.getElementById("downloadBox")
+  const btn = document.getElementById("downloadBtn")
+
+  box.classList.add("hidden")
+
+  if(!link){
+    status.innerText = "Masukkan link YouTube dulu."
+    return
+  }
+
+  status.innerText = "⏳ Sedang convert..."
+
+  try{
+
+    const res = await fetch(`/api/convert?type=${type}&url=${encodeURIComponent(link)}`)
+    const data = await res.json()
+
+    if(data.download){
+      status.innerText = "✅ Convert selesai!"
+      btn.href = data.download
+      box.classList.remove("hidden")
+    }else{
+      status.innerText = "❌ Gagal convert."
+    }
+
+  }catch(err){
+    status.innerText = "❌ Error server."
+  }
+}
+
